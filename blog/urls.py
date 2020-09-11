0636aa817d7959881cb88128b219a21b1a5fc7e0
smartcodes-tz <lsuleiman2002@gmail.com>
2020-09-11 23:33:13 +0300

    fixed urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from blog import views
 
 urlpatterns = [
     path('blog/', views.BlogPageList.as_view(), name='blog'),
-    path('post/new', views.addpost, name='post_form'),
-    path('<slug:slug>/', views.detail, name='post_detail'),
+    path('post/new/', views.addpost, name='post_form'),
+    path('blog/<slug:slug>/', views.detail, name='post_detail'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('post/dashboard/', views.dashboard, name='dashboard'),
-    path('post/<slug:slug>/update', views.updatepost, name='update_post'),
-    path('post/<slug:slug>/delete', views.deletepost, name='delete_post'),
+    path('post/<slug:slug>/update/', views.updatepost, name='update_post'),
+    path('post/<slug:slug>/delete/', views.deletepost, name='delete_post'),
 ]
