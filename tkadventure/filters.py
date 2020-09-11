fb6530e2a47fa8b564e3eefa5b8cf166aca4f027
smartcodes-tz <lsuleiman2002@gmail.com>
2020-09-11 10:49:21 +0300

    fixing filters

--- tkadventure/filters.py
+++ tkadventure/filters.py
@@ -6,6 +6,5 @@ class TourFilter(django_filters.FilterSet):
     class Meta:
         model = Tour
         fields = {
-            'name': ['icontains'],
-            'tour_type': ['icontains']
+            'name': ['icontains']
         }
