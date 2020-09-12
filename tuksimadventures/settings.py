0e1bf772da9a698eeba6ec50b459d178fce8b2cd
smartcodes-tz <lsuleiman2002@gmail.com>
2020-09-12 14:55:28 +0300

    fixing aws

--- tuksimadventures/settings.py
+++ tuksimadventures/settings.py
@@ -129,7 +129,7 @@ AWS_STORAGE_BUCKET_NAME = 'tuksim-buckets'
 # AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_QUERYSTRING_AUTH = False
+
 
 
 STATIC_URL = '/static/'
